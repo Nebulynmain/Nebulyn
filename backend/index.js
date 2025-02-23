53272431ec14dc62a9a1f8c62f22392fd9dae5d3
Sumit <sumit1711kd@gmail.com>
2025-02-23 13:00:05 +0530

    Add cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv'
 import connect from './config/db.js';
 import authRouter from './routes/authRoute.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
 dotenv.config();
 const PORT = process.env.PORT || 8000 ;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
